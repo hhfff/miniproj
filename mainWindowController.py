f1e95ae4e5ef94ba5adf7114b784beabf188b8c3
hhfxxx <hefei.he95@gmail.com>
2019-10-03 16:53:14 +0800

    a

---
+++ mainWindowController.py
@@ -0,0 +1,17 @@
+import datetime
+import calendar
+class MapytinWindowController():
+    def __init__(self):
+        self.currentDatetime=self.getCurrentSystemTime()
+        self.selectedDateTime=datetime.fromtimestamp(self.currentDatetime.)
+
+    def getCurrentSystemTime(self):
+        return datetime.datetime.now()
+    
+    def getDayNameByDate(self,datetime):
+        #monday is 0, but in database is 1 so plus 1
+        return datetime.weekday()+1
+
+
+    
+        
