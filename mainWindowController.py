c7271ce01c19fa658e20abfe8a798790c3481662
Stanley Ho <50760067+beaniestanley@users.noreply.github.com>
2019-10-08 00:44:12 +0800

    Debugged, update with test case

--- mainWindowController.py
+++ mainWindowController.py
@@ -3,15 +3,16 @@ import calendar
 class MapytinWindowController():
     def __init__(self):
         self.currentDatetime=self.getCurrentSystemTime()
-        self.selectedDateTime=datetime.fromtimestamp(self.currentDatetime.)
+        self.selectedDateTime= datetime.datetime.fromtimestamp(self.currentDatetime)
 
     def getCurrentSystemTime(self):
         return datetime.datetime.now()
     
-    def getDayNameByDate(self,datetime):
+    def getDayNameByDate(self):
         #monday is 0, but in database is 1 so plus 1
         return datetime.weekday()+1
 
-
-    
+datetime = datetime.date(year= 2019, month= 10, day= 3)
+print(MapytinWindowController.getDayNameByDate(datetime))
+#output is 4 (meaning Thursday)    
         
