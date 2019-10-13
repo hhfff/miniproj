37e384e42a7208e7b8c2d7d901c9c4df2ee3aa28
Stanley Ho <50760067+beaniestanley@users.noreply.github.com>
2019-10-13 19:08:39 +0800

    Update SelectDateTime.py

--- SelectDateTime.py
+++ SelectDateTime.py
@@ -1,12 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\SelectDateTime.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
 from datetime import datetime
 from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(591, 367)
-        self.calendarWidget = QtWidgets.QCalendarWidget(Dialog)
-        self.calendarWidget.setGeometry(QtCore.QRect(10, 50, 361, 261))
+from PyQt5.QtWidgets import QCalendarWidget
+
+#codes for this class was derived from Qt Designer
+class Ui_SelectDateTime(object):
+    def setupUi(self, SelectDateTime):
+        SelectDateTime.setObjectName("SelectDateTime")
+        SelectDateTime.resize(644, 413)
+        SelectDateTime.setMaximumSize(QtCore.QSize(645, 413))
+        self.calendarWidget = QtWidgets.QCalendarWidget(SelectDateTime)
+        self.calendarWidget.setGeometry(QtCore.QRect(30, 50, 361, 261))
         font = QtGui.QFont()
         font.setFamily("Trebuchet MS")
         font.setPointSize(11)
@@ -21,8 +33,8 @@ class Ui_Dialog(object):
         self.calendarWidget.setHorizontalHeaderFormat(QtWidgets.QCalendarWidget.ShortDayNames)
         self.calendarWidget.setVerticalHeaderFormat(QtWidgets.QCalendarWidget.NoVerticalHeader)
         self.calendarWidget.setObjectName("calendarWidget")
-        self.titleDate = QtWidgets.QLabel(Dialog)
-        self.titleDate.setGeometry(QtCore.QRect(90, 20, 201, 21))
+        self.titleDate = QtWidgets.QLabel(SelectDateTime)
+        self.titleDate.setGeometry(QtCore.QRect(110, 20, 201, 21))
         font = QtGui.QFont()
         font.setFamily("Eras Demi ITC")
         font.setPointSize(14)
@@ -34,8 +46,19 @@ class Ui_Dialog(object):
         font.setStyleStrategy(QtGui.QFont.PreferDefault)
         self.titleDate.setFont(font)
         self.titleDate.setObjectName("titleDate")
-        self.timeEdit = QtWidgets.QTimeEdit(Dialog)
-        self.timeEdit.setGeometry(QtCore.QRect(410, 180, 151, 41))
+        self.titleTime = QtWidgets.QLabel(SelectDateTime)
+        self.titleTime.setGeometry(QtCore.QRect(420, 140, 201, 21))
+        font = QtGui.QFont()
+        font.setFamily("Eras Demi ITC")
+        font.setPointSize(14)
+        font.setBold(False)
+        font.setWeight(50)
+        self.titleTime.setFont(font)
+        self.titleTime.setTextFormat(QtCore.Qt.RichText)
+        self.titleTime.setWordWrap(False)
+        self.titleTime.setObjectName("titleTime")
+        self.timeEdit = QtWidgets.QTimeEdit(SelectDateTime)
+        self.timeEdit.setGeometry(QtCore.QRect(450, 180, 151, 41))
         font = QtGui.QFont()
         font.setFamily("Trebuchet MS")
         font.setPointSize(10)
@@ -49,101 +72,59 @@ class Ui_Dialog(object):
         self.timeEdit.setCurrentSectionIndex(0)
         self.timeEdit.setTimeSpec(QtCore.Qt.LocalTime)
         self.timeEdit.setTime(QtCore.QTime(12, 0, 0))
-        self.timeEdit.setObjectName("timeEdit")
-        self.titleTime = QtWidgets.QLabel(Dialog)
-        self.titleTime.setGeometry(QtCore.QRect(380, 150, 201, 21))
-        font = QtGui.QFont()
-        font.setFamily("Eras Demi ITC")
-        font.setPointSize(14)
-        font.setBold(False)
-        font.setWeight(50)
-        self.titleTime.setFont(font)
-        self.titleTime.setTextFormat(QtCore.Qt.RichText)
-        self.titleTime.setWordWrap(False)
-        self.titleTime.setObjectName("titleTime")
-        self.pushButtonBack = QtWidgets.QPushButton(Dialog)
-        self.pushButtonBack.setGeometry(QtCore.QRect(50, 320, 211, 41))
-        palette = QtGui.QPalette()
-        brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.WindowText, brush)
-        brush = QtGui.QBrush(QtGui.QColor(170, 255, 255))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Base, brush)
-        brush = QtGui.QBrush(QtGui.QColor(20, 255, 149))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Window, brush)
-        brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.WindowText, brush)
-        brush = QtGui.QBrush(QtGui.QColor(170, 255, 255))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Base, brush)
-        brush = QtGui.QBrush(QtGui.QColor(20, 255, 149))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Window, brush)
-        brush = QtGui.QBrush(QtGui.QColor(120, 120, 120))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.WindowText, brush)
-        brush = QtGui.QBrush(QtGui.QColor(20, 255, 149))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Base, brush)
-        brush = QtGui.QBrush(QtGui.QColor(20, 255, 149))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Window, brush)
-        self.pushButtonBack.setPalette(palette)
+        self.pushButtonBack = QtWidgets.QPushButton(SelectDateTime)
+        self.pushButtonBack.setGeometry(QtCore.QRect(80, 360, 211, 41))
         self.pushButtonBack.setObjectName("pushButtonBack")
-        self.pushButtonConfirm = QtWidgets.QPushButton(Dialog)
-        self.pushButtonConfirm.setGeometry(QtCore.QRect(320, 320, 221, 41))
+        self.pushButtonConfirm = QtWidgets.QPushButton(SelectDateTime)
+        self.pushButtonConfirm.setGeometry(QtCore.QRect(380, 360, 221, 41))
         self.pushButtonConfirm.setObjectName("pushButtonConfirm")
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(SelectDateTime)
+        QtCore.QMetaObject.connectSlotsByName(SelectDateTime)
 
-
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, SelectDateTime):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Canteen System"))
-        self.titleDate.setText(_translate("Dialog", "Set Preferred Date"))
-        self.timeEdit.setDisplayFormat(_translate("Dialog", "hh:mm AP"))
-        self.titleTime.setText(_translate("Dialog", "Set Preferred Time"))
-        self.pushButtonBack.setText(_translate("Dialog", "Back"))
-        self.pushButtonConfirm.setText(_translate("Dialog", "Confirm"))
+        SelectDateTime.setWindowTitle(_translate("SelectDateTime", "Canteen System"))
+        self.titleDate.setText(_translate("SelectDateTime", "Set Preferred Date"))
+        self.titleTime.setText(_translate("SelectDateTime", "Set Preferred Time"))
+        self.timeEdit.setDisplayFormat(_translate("SelectDateTime", "hh:mm AP"))
+        self.pushButtonBack.setText(_translate("SelectDateTime", "Back"))
+        self.pushButtonConfirm.setText(_translate("SelectDateTime", "Confirm"))
 
-
-class OpenSelectDateTimeDialog(QtWidgets.QDialog, Ui_Dialog, QtCore.QDate):
-    global currentDay, currentMonth, currentYear, chosenDay, chosenMonth,chosenYear
+#create new class to open GUI generated from Qt Designer
+class OpenSelectDateTimeWindow(QtWidgets.QWidget, Ui_SelectDateTime, QtCore.QDate):
+    #set global main day, month, year
+    global currentDay, currentMonth, currentYear
 
     currentDay = datetime.now().day
     currentMonth = datetime.now().month
     currentYear = datetime.now().year
-
-
+    
+    #initialise GUI and window
     def __init__(self):
-        QtWidgets.QDialog.__init__(self)
+        QtWidgets.QWidget.__init__(self)
         QtCore.QDate.__init__(self)
         self.setupUi(self)
-        self.pushButtonBack.clicked.connect(self.hide)
-        #print default date for cases where user does not change date
-        print(self.calendarWidget.selectedDate().toString("dd-MM-yyyy"))
-        self.calendarWidget.clicked.connect(self.userSelectedDate)
+        #when Back button is clicked, window is closed
+        self.pushButtonBack.clicked.connect(self.close)
+        #when Confirm button is clicked, return date chosen by the user
+        self.pushButtonConfirm.clicked.connect(self.userSelectedDate)
 
     #2 functions to return selected date and time on the
     #MainWindow UI / New Window UI where stall's information
     #will be printed according to the user chosen date and time
+    
+    #this function is to return user chosen date
     def userSelectedDate(self):
         self.date = self.calendarWidget.selectedDate().toString("dd-MM-yyyy")
         print(self.date)
         return self.date
-
+    
     #def userChosenTime(self):
 
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
-    SelectDateTimeDialog = OpenSelectDateTimeDialog()
-    SelectDateTimeDialog.show()
+    SelectDateTimeWindow = OpenSelectDateTimeWindow()
+    SelectDateTimeWindow.show()
     sys.exit(app.exec_())
-
-
