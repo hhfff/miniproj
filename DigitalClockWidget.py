a4136bc69cdfb739e5cdb9da86fd047af4dc3166
Stanley Ho <50760067+beaniestanley@users.noreply.github.com>
2019-11-08 20:37:07 +0800

    Update DigitalClockWidget.py to work in main_ui
    Still need to create a grid for this and a separate grid for chosen date and time, method to call the widge can be found in Stall_Info_Page lines 73 to 86

--- DigitalClockWidget.py
+++ DigitalClockWidget.py
@@ -10,10 +10,11 @@ class Ui_widget_DigitalClock(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(widget_DigitalClock.sizePolicy().hasHeightForWidth())
         widget_DigitalClock.setSizePolicy(sizePolicy)
+
         self.verticalLayout = QtWidgets.QVBoxLayout(widget_DigitalClock)
         self.verticalLayout.setObjectName("verticalLayout")
 
-        self.label_Title = QtWidgets.QLabel(widget_DigitalClock)
+        '''self.label_Title = QtWidgets.QLabel(widget_DigitalClock)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding,
                                            QtWidgets.QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(0)
@@ -29,9 +30,9 @@ class Ui_widget_DigitalClock(object):
         self.label_Title.setAlignment(QtCore.Qt.AlignCenter)
         self.label_Title.setObjectName("label_Title")
         self.label_Title.setText("Current Date and Time:")
-        self.verticalLayout.addWidget(self.label_Title)
+        self.verticalLayout.addWidget(self.label_Title)'''
 
-        self.label_CurrentDate = QtWidgets.QLabel(widget_DigitalClock)
+        '''self.label_CurrentDate = QtWidgets.QLabel(widget_DigitalClock)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -45,7 +46,7 @@ class Ui_widget_DigitalClock(object):
         self.label_CurrentDate.setFont(font)
         self.label_CurrentDate.setAlignment(QtCore.Qt.AlignCenter)
         self.label_CurrentDate.setObjectName("label_CurrentDate")
-        self.verticalLayout.addWidget(self.label_CurrentDate)
+        self.verticalLayout.addWidget(self.label_CurrentDate)'''
 
         self.label_DigitalClock = QtWidgets.QLabel(widget_DigitalClock)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.MinimumExpanding)
@@ -55,7 +56,7 @@ class Ui_widget_DigitalClock(object):
         self.label_DigitalClock.setSizePolicy(sizePolicy)
         font = QtGui.QFont()
         font.setFamily("Palatino Linotype")
-        font.setPointSize(11)
+        font.setPointSize(10)
         font.setBold(True)
         font.setWeight(75)
         self.label_DigitalClock.setFont(font)
@@ -69,45 +70,42 @@ class Ui_widget_DigitalClock(object):
     def retranslateUi(self, widget_DigitalClock):
         _translate = QtCore.QCoreApplication.translate
         widget_DigitalClock.setWindowTitle(_translate("widget_DigitalClock", "Canteen System"))
-        self.label_CurrentDate.setText(_translate("widget_DigitalClock", "Current Date"))
+        #self.label_CurrentDate.setText(_translate("widget_DigitalClock", "Current Date"))
         self.label_DigitalClock.setText(_translate("widget_DigitalClock", "Current Time"))
 
 
 class DigitalClock(QtWidgets.QWidget, Ui_widget_DigitalClock):
-    def __init__(self):
+    def __init__(self,mainWindowController):
         super(DigitalClock, self).__init__()
         self.setupUi(self)
+        self.mainWindowController = mainWindowController
 
         # open QTimer class to connect showDate and showTime functions
         # this allows the date and time to update itself in constant intervals
         timer = QtCore.QTimer(self)
-        timer.timeout.connect(self.showDate)
-        timer.timeout.connect(self.showTime)
+        timer.timeout.connect(self.showDateAndTime)
         timer.start(100)
 
     # get the current date and convert it to string of the applied format, and print it on the label
-    def showDate(self):
+    def showDateAndTime(self):
         # get current date
         date = QtCore.QDate.currentDate()
         # convert today's date into a string of the format day, date month, year
-        # example (Wednesday, 06 November, 2019)
-        textDate = date.toString("dddd, dd MMMM, yyyy")
-        # update label_CurrentDate with textDate string
-        self.label_CurrentDate.setText(textDate)
-
-    def showTime(self):
+        # example 06/11/2019
+        textDate = date.toString("dddd dd/MM/yyyy")
         # get current time
         time = QtCore.QTime.currentTime()
         # convert current time into a string of the format in hours: minutes: seconds
-        # example (16:20:15)
+        # example 16:20:15
         textTime = time.toString('hh:mm:ss')
-        # update label_DigitalClock with textTime string
-        self.label_DigitalClock.setText(textTime)
+        # update label_DigitalClock with textDate and textTime string
+        # example 06/11/2019 16:20:15
+        self.label_DigitalClock.setText('Current Date and Time: ' + textDate + ' ' + textTime)
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    DigitalClockWidget = DigitalClock()
+    DigitalClockWidget = DigitalClock(mainWindowController=None)
     DigitalClockWidget.show()
     sys.exit(app.exec_())
